afce84382be15b75fbe3cbc2275c4aa579643120
Lex Ibanez <laibanez@uci.edu>
2024-02-12 21:36:14 -0800

    added exception handling

--- wp2.py
+++ wp2.py
@@ -1,9 +1,10 @@
 import sys
 from pathlib import Path
 from encrypt import *
+from keyvalidate import _validate_key, InvalidKeyException
 
-# python wp2.py -e original.txt encrypted.txt 4
-# python wp2.py -d encrypted.txt decrypted.txt 4
+# python wp2.py -e original.txt encrypted.txt 11
+# python wp2.py -d encrypted.txt decrypted.txt 11
 
 def load_files(file1_txt, file2_txt):
     file1 = Path(file1_txt)
@@ -31,20 +32,26 @@ def get_input():
         print("Error. Usage: python wp2.py command file.txt file.txt key")
 
 def main():
-    command, file1_txt, file2_txt, key = get_input()
-    file1, file2 = load_files(file1_txt, file2_txt)
+    try:
+        command, file1_txt, file2_txt, key = get_input()
+        file1, file2 = load_files(file1_txt, file2_txt)
 
-    if command == '-e':
-        encrypted_file = encrypt_message(file1, file2, key)
-        with open(encrypted_file, 'r') as file:
-            for line in file:
-                print(line.strip())
-                      
-    elif command == '-d':
-        decrypted_file = decrypt_message(file1, file2, key)
-        with open(decrypted_file, 'r') as file:
-            for line in file:
-                print(line.strip())
+        test_key = _validate_key(key, file1)
+
+        if command == '-e':
+            encrypted_file = encrypt_message(file1, file2, key)
+            with open(encrypted_file, 'r') as file:
+                for line in file:
+                    print(line.strip())
+
+        elif command == '-d':
+            decrypted_file = decrypt_message(file1, file2, key)
+            with open(decrypted_file, 'r') as file:
+                for line in file:
+                    print(line.strip())
+
+    except InvalidKeyException as e:
+        print(e)
 
 if __name__ == '__main__':
     main()
